2f0ed3a3e65ab9656ca177457eb996062acaf619
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-05-19 22:35:06 -0400

    Add logging sys.excepthook

--- src/basicpyqt5example/__main__.py
+++ src/basicpyqt5example/__main__.py
@@ -19,7 +19,40 @@ __license__ = 'GPLv3+'
 #   <http://www.gnu.org/licenses/>.
 
 
+import logging
 import sys
+import pathlib
+import traceback
+
+logger = logging.getLogger(__name__)
+
+# Consider if you want a fresh file each time or a running log.
+# mode='w' -> fresh file
+# mode='a' (or no mode specified) -> running log
+file_handler = logging.FileHandler('log', mode='w', encoding='utf-8')
+logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+
+
+def excepthook(excType=None, excValue=None, tracebackobj=None):
+    logger.critical(''.join(traceback.format_exception(
+        etype=excType,
+        value=excValue,
+        tb=tracebackobj,
+    )).strip())
+
+
+sys.excepthook = excepthook
+
+logger.critical('Logging sys.excepthook installed')
+logger.critical('Import in progress for {}'.format(pathlib.Path(__file__).resolve()))
+logger.critical('sys.argv: {}'.format(sys.argv))
+
+# Uncomment to test that you can at least see this in the logfile
+# raise Exception('you should see this')
+
 
 import PyQt5.QtWidgets
 
